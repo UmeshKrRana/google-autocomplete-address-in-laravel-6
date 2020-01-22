7992390e543549043f52184dc9d424bc2ba71b7b
UmeshKrRana <umesh.rana0269@gmail.com>
2020-01-22 22:02:17 +0530

    updates

--- resources/views/auto-complete.blade.php
+++ resources/views/auto-complete.blade.php
@@ -44,7 +44,7 @@
 
 
 {{-- javascript code --}}
-   <script src="https://maps.google.com/maps/api/js?key=AIzaSyDxTV3a6oL6vAaRookXxpiJhynuUpSccjY&libraries=places&callback=initAutocomplete" type="text/javascript"></script>
+   <script src="https://maps.google.com/maps/api/js?key=&libraries=places&callback=initAutocomplete" type="text/javascript"></script>
 
    <script>
        $(document).ready(function() {
